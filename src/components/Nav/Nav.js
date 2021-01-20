1ac3f259abbca51095a42ff86ad567b0005712bb
Alyssa DeMetro <leanndemetro@gmail.com>
2021-01-20 08:18:49 -0500

    image added to dashboard page, dashboardimg component created but not functionally routed to app.js, image rendering from navbar component

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,29 +1,26 @@
 import React, { Component } from "react";
-import { MDBNavbar, MDBNavbarBrand, MDBNavbarNav, MDBNavItem, MDBNavLink, MDBNavbarToggler, MDBCollapse, MDBDropdown,
-MDBDropdownToggle, MDBDropdownMenu, MDBDropdownItem, MDBIcon } from "mdbreact";
+import {
+  MDBNavbar, MDBNavbarBrand, MDBNavbarNav, MDBNavItem, MDBNavLink, MDBNavbarToggler, MDBCollapse, MDBDropdown,
+  MDBDropdownToggle, MDBDropdownMenu, MDBDropdownItem, MDBIcon
+} from "mdbreact";
 import { BrowserRouter as Router } from 'react-router-dom';
 import "./style.css"
 
 class NavbarPage extends Component {
-state = {
-  isOpen: false
-};
+  
 
-toggleCollapse = () => {
-  this.setState({ isOpen: !this.state.isOpen });
-}
+  render() {
+    return (
+      <Router>
+        <MDBNavbar color="white" dark expand="md">
+          <MDBNavbarBrand>
+            <img className="growImg" src="https://i.imgur.com/gCIu3Wl.png"></img>
+          </MDBNavbarBrand>
+        </MDBNavbar>
+        <img className="homeImg" src="https://www.groworlando.org/wp-content/uploads/2018/11/agriculture-backyard-blur-296230-e1543305529365.jpg"></img>
+      </Router>
 
-render() {
-  return (
-    <Router>
-      <MDBNavbar color="white" dark expand="md">
-        <MDBNavbarBrand>
-          <img className="growImg" src="https://i.imgur.com/gCIu3Wl.png"></img>
-        </MDBNavbarBrand>
-        
-        
-      </MDBNavbar>
-    </Router>
+
     );
   }
 }
---
+++ src/components/dashboardImg/dashboardImg.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class dashboardImg extends Component {
+    render() {
+        return (
+           
+         <div>
+            <img className="homeImg" src="https://www.groworlando.org/wp-content/uploads/2018/11/agriculture-backyard-blur-296230-e1543305529365.jpg"></img>
+            </div>
+        
+          );
+        }
+}
+
+export default dashboardImg;
