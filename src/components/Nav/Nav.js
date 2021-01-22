e7ad6f2993af99636cae6abb682e201fa6ed0de2
Alyssa DeMetro <leanndemetro@gmail.com>
2021-01-22 16:06:15 -0500

    navbar links added to navbar

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import {
-  MDBNavbar, MDBNavbarBrand, MDBNavbarNav, MDBNavItem, MDBNavLink, MDBNavbarToggler, MDBCollapse, MDBDropdown,
+  MDBNavbar, MDBNavbarBrand, MDBNavbarNav, MDBNav, MDBNavItem, MDBNavLink, MDBNavbarToggler, MDBCollapse, MDBDropdown,
   MDBDropdownToggle, MDBDropdownMenu, MDBDropdownItem, MDBIcon
 } from "mdbreact";
 import { BrowserRouter as Router } from 'react-router-dom';
@@ -16,6 +16,21 @@ class NavbarPage extends Component {
           <MDBNavbarBrand>
             <img className="growImg" src="https://i.imgur.com/gCIu3Wl.png"></img>
           </MDBNavbarBrand>
+          <div className="links"><MDBNav className="justify-content-end">
+      <MDBNavItem>
+        <MDBNavLink className="black-text" active to="#!">Active</MDBNavLink>
+      </MDBNavItem>
+      <MDBNavItem>
+        <MDBNavLink className="black-text" to="#!">Link</MDBNavLink>
+      </MDBNavItem>
+      <MDBNavItem>
+        <MDBNavLink className="black-text" to="#!">Link</MDBNavLink>
+      </MDBNavItem>
+      <MDBNavItem>
+        <MDBNavLink className="black-text" to="#!">Link</MDBNavLink>
+      </MDBNavItem>
+    </MDBNav>
+    </div>
         </MDBNavbar>
         <img className="homeImg" src="https://www.groworlando.org/wp-content/uploads/2018/11/agriculture-backyard-blur-296230-e1543305529365.jpg"></img>
       </Router>
--- src/components/dashboardImg/dashboardImg.js
+++ src/components/dashboardImg/dashboardImg.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import "./style.css";
 
 class dashboardImg extends Component {
     render() {
