0bbe915bc7c80612f852087de1f4893050dc6d9a
Alyssa DeMetro <leanndemetro@gmail.com>
2021-01-22 14:42:52 -0500

    pushing for askBCS assistance

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,12 +1,13 @@
 import React, { Component } from "react";
 import NavbarPage from "../components/Nav/Nav";
-import dashboardImg from "../components/dashboardImg/dashboardImg"
+
 
 
 class Dashboard extends Component {
   render() {
     return (
       <NavbarPage />
+    
 
       
 
