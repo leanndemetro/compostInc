c80efc9657b4c48dc119c4edf0f8b6802d1e8c91
Alyssa DeMetro <leanndemetro@gmail.com>
2021-01-23 12:59:57 -0500

    navbar asthetic complete, links unworking

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,47 +1,50 @@
 import React, { Component } from "react";
 import {
   MDBNavbar, MDBNavbarBrand, MDBNavbarNav, MDBNav, MDBNavItem, MDBNavLink, MDBNavbarToggler, MDBCollapse, MDBDropdown,
-  MDBDropdownToggle, MDBDropdownMenu, MDBDropdownItem, MDBIcon
+  MDBDropdownToggle, MDBDropdownMenu, MDBDropdownItem, MDBIcon, MDBContainer, MDBRow, MDBCol
 } from "mdbreact";
 import { BrowserRouter as Router } from 'react-router-dom';
 import "./style.css"
 
 class NavbarPage extends Component {
-  
+
 
   render() {
     return (
       <Router>
-        <MDBNavbar color="white" dark expand="md">
+        <MDBNavbar color="AFF7C0" dark expand="md">
           <MDBNavbarBrand>
             <a href="/"><img className="growImg" src="https://i.imgur.com/nd70Zb3.jpg"></img></a>
           </MDBNavbarBrand>
+          <MDBContainer>
+            <MDBCol></MDBCol>
           <div className="links"><MDBNav className="font-weight-bold">
-      <MDBNavItem>
-        <MDBNavLink className="black-text" active to="/Contact">CONTACT</MDBNavLink>
-      </MDBNavItem>
-      <MDBNavItem>
-        <MDBNavLink className="black-text" to="/About">ABOUT</MDBNavLink>
-      </MDBNavItem>
-      <MDBNavItem>
-        <MDBNavLink className="black-text" to="/Donate">DONATE</MDBNavLink>
-      </MDBNavItem>
-      <MDBNavItem>
-        <MDBNavLink className="black-text" to="/Help">HELP</MDBNavLink>
-      </MDBNavItem>
-      <MDBNavItem>
-        <MDBNavLink className="black-text" to="/Ig"><MDBIcon fab icon="instagram" /></MDBNavLink>
-      </MDBNavItem>
-      <MDBNavItem>
-        <MDBNavLink className="black-text" to="https://www.facebook.com/GrowOrlandoInc/"><MDBIcon fab icon="facebook-square" /></MDBNavLink>
-      </MDBNavItem>
-    </MDBNav>
-    </div>
+            <MDBNavItem>
+              <MDBNavLink className="black-text" active to="/Contact">CONTACT</MDBNavLink>
+            </MDBNavItem>
+            <MDBNavItem>
+              <MDBNavLink className="black-text" to="/About">ABOUT</MDBNavLink>
+            </MDBNavItem>
+            <MDBNavItem>
+              <MDBNavLink className="black-text" to="/Donate">DONATE</MDBNavLink>
+            </MDBNavItem>
+            <MDBNavItem>
+              <MDBNavLink className="black-text" to="/Help">HELP</MDBNavLink>
+            </MDBNavItem>
+            <MDBNavItem>
+              <MDBNavLink className="black-text" to="/Ig"><MDBIcon fab icon="instagram" /></MDBNavLink>
+            </MDBNavItem>
+            <MDBNavItem>
+              <MDBNavLink className="black-text" to="https://www.facebook.com/GrowOrlandoInc/"><MDBIcon fab icon="facebook-square" /></MDBNavLink>
+            </MDBNavItem>
+          </MDBNav>
+          </div>
+          </MDBContainer>
         </MDBNavbar>
-       
-    
+
+          
         <img className="homeImg" src="https://i.imgur.com/zzjyKfm.jpg"></img>
-       
+
       </Router>
 
 
