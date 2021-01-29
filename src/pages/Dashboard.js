1c755f3e17338e345cc273f4d1bfcb1dbe1c16b4
Alyssa DeMetro <leanndemetro@gmail.com>
2021-01-29 12:42:32 -0500

    above <hr> tag finalized other than font

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -18,15 +18,21 @@ function Dashboard() {
               </MDBRow>
               <MDBRow>
                 <MDBCol>
-                <div className="linkImg"><img className="TextWrap img-fluid" src="https://images.unsplash.com/photo-1566040924976-f837330d1a5b?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1267&q=80" waves />
-                    <p className="mainText"> Turnip greens yarrow ricebean rutabaga endive cauliflower sea lettuce kohlrabi amaranth water spinach avocado daikon napa cabbage asparagus winter purslane kale. Celery potato scallion desert raisin horseradish spinach carrot soko. Lotus root water spinach fennel kombu maize bamboo shoot green bean swiss chard seakale pumpkin onion chickpea gram corn pea. Brussels sprout coriander water chestnut gourd swiss chard wakame kohlrabi beetroot carrot watercress. Corn amaranth salsify bunya nuts nori azuki bean chickweed potato bell pepper artichoke.Lotus root water spinach fennel kombu maize bamboo shoot green bean swiss chard seakale pumpkin onion chickpea gram corn pea. Brussels sprout coriander water chestnut gourd swiss chard wakame kohlrabi beetroot carrot watercress. Corn amaranth salsify bunya nuts nori azuki bean chickweed potato bell pepper artichoke.</p>
+                <div className="linkImg"><img className="TextWrap img-fluid" src="https://i.imgur.com/alIHZq3.jpg" waves />
+                    <br></br> 
+                    <p className="mainText"> Turnip greens yarrow ricebean rutabaga endive cauliflower sea lettuce kohlrabi amaranth water spinach avocado daikon napa cabbage asparagus winter purslane kale. Celery potato scallion desert raisin horseradish spinach carrot soko. Lotus root water spinach fennel kombu maize bamboo shoot green bean swiss chard seakale pumpkin onion chickpea gram corn pea.
+                    <br></br> 
+                    <br></br> 
+                    Brussels sprout coriander water chestnut gourd swiss chard wakame kohlrabi beetroot carrot watercress. Corn amaranth salsify bunya nuts nori azuki bean chickweed potato bell pepper artichoke.Lotus root water spinach fennel kombu maize bamboo shoot green bean swiss chard seakale pumpkin onion chickpea gram corn pea. Brussels sprout coriander water chestnut gourd swiss chard wakame kohlrabi beetroot carrot watercress. Corn amaranth salsify.</p>
                      </div>
                 </MDBCol>
               </MDBRow>
               <MDBRow>
                 <MDBCol>
-                <a href="/HowItWorks"><p className="img__description">HOW IT WORKS</p></a>
-                </MDBCol>
+                  <ul>
+                <a href="/HowItWorks"><p className="howLink"><li>HOW IT WORKS</li></p></a>
+                </ul>
+          </MDBCol>
               </MDBRow>
               <MDBRow>
                 <MDBCol>
