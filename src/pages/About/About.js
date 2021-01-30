5f25dbe6af7fc2692f2f95795281a94ed4cd031b
Alyssa DeMetro <leanndemetro@gmail.com>
2021-01-30 14:38:20 -0500

    cosmetic changes

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -23,16 +23,16 @@ function About ()  {
           <MDBCol lg="6" md="12" className="mb-5">
             <MDBCol md="4" lg="6" className="float-left">
               <img
-                src="https://mdbootstrap.com/img/Photos/Avatars/img%20(32).jpg"
+                src="https://i.imgur.com/ctYdCVL.jpg"
                 className="mx-auto mb-md-0 mb-4 rounded z-depth-1 img-fluid"
                 tag="img"
-                alt="Sample avatar"
+                alt="FrankBaily"
               />
             </MDBCol>
             <MDBCol md="8" lg="6" className="float-right">
-              <h4 className="font-weight-bold mb-3">John Doe</h4>
+              <h4 className="font-weight-bold mb-3">FRANK BAILEY</h4>
               <h6 className="font-weight-bold grey-text mb-3">
-                Web Designer
+                Owner
               </h6>
               <p className="black-text">
                 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod
@@ -60,7 +60,7 @@ function About ()  {
               />
             </MDBCol>
             <MDBCol md="8" lg="6" className="float-right">
-              <h4 className="font-weight-bold mb-3">Maria Kate</h4>
+              <h4 className="font-weight-bold mb-3">MARIA KATE</h4>
               <h6 className="font-weight-bold grey-text mb-3">
                 Photographer
               </h6>
@@ -90,7 +90,7 @@ function About ()  {
               />
             </MDBCol>
             <MDBCol md="8" lg="6" className="float-right">
-              <h4 className="font-weight-bold mb-3">Anna Deynah</h4>
+              <h4 className="font-weight-bold mb-3">ANNA DEYNAH</h4>
               <h6 className="font-weight-bold grey-text mb-3">
                 Web Developer
               </h6>
@@ -120,7 +120,7 @@ function About ()  {
               />
             </MDBCol>
             <MDBCol md="8" lg="6" className="float-right">
-              <h4 className="font-weight-bold mb-3">Sarah Melyah</h4>
+              <h4 className="font-weight-bold mb-3">SARAH MELYAH</h4>
               <h6 className="font-weight-bold grey-text mb-3">
                 Front-end Developer
               </h6>
