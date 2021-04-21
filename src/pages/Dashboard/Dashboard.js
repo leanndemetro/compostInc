78af9f3cd2c65702a56a2f98f57d3418904f7642
Alyssa DeMetro <leanndemetro@gmail.com>
2021-04-21 18:01:46 -0400

    links working cosmetic updated

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -16,7 +16,7 @@ function Dashboard() {
               <h1 className="dashText">Help us use plants to grow the lives of local teens!</h1>
               <p className="dashTextTwo">Here at Grow Orlando it is our mission to connect students with the world around them. Through access to our permaculture based local gardens, and training programs, we aim to educate students with lifelong gardening or even job skills.</p>
               <br></br>
-              <MDBBtn className="learnMore" color="white">learn more</MDBBtn>
+              <MDBBtn className="learnMore" color="white"><a href="/howItWorks">learn more</a></MDBBtn>
               </div>
       </MDBCol>
       </MDBRow>
