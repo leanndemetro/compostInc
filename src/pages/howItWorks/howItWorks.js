6a8761d44b483e29176221db3f3b240266f270d6
Alyssa DeMetro <leanndemetro@gmail.com>
2021-04-21 17:46:26 -0400

    what we do page built out with general mobile respo responsiveness complete but imperfect

--- src/pages/howItWorks/howItWorks.js
+++ src/pages/howItWorks/howItWorks.js
@@ -1,8 +1,6 @@
 import React from "react";
 import {
-  MDBContainer, MDBRow, MDBCol, MDBCard
-} from "mdbreact";
-import Carousel from "../../components/Carousel/Carousel"
+  MDBContainer, MDBBtn, MDBRow, MDBCol} from "mdbreact";
 import "./style.css"
 
 
@@ -11,49 +9,36 @@ import "./style.css"
 
 function HowItWorks ()  {
     return (
-        <MDBContainer>
-        <MDBRow>
-          <MDBCol>
-              <MDBContainer>
-            <p className="howWorks">WHAT IS COMPOST?</p>
-            <br></br>
-            <br></br>
-            <img className="howTextWrap" src="https://i.imgur.com/RSEJOnz.jpg?1"></img>
-            <p className="howTextTop">Anaerobic digestion is a sequence of processes by which microorganisms break down biodegradable material in the absence of oxygen. The process is used for industrial or domestic purposes to manage waste or to produce fuels. 
-            <br></br>
-            <br></br>
-            We use this technology to decrease food and animal wastes within our community by converting them to compost to be used as fertilizer or fuel.</p>
-            <br></br>
-            <br></br>
-            <br></br>
-            <p className="howWorks">HOW DOES IT WORK?</p>
-            <br></br>
-            <br></br>
-            <img className="howTextWrapLeft" src="https://i.imgur.com/VO42P76.jpg?1"></img>
-            <ul>
-            <li><p className="howText">Upon contact, we will schedule a date for a site visit. Our technicians will determine the best course of action to take in order to meet your particular installation needs.
-            <br></br>
-            <br></br>
-           Dates for installation and training will be decided at the conclusion of this site visit. </p>
-            </li>
-            <br></br>
-            <li><p className="howText"> Our team will install your anerobic digester, train your staff, students, or volunteers, take care of ALL maintenence, and waste removal - completely free of charge! 
-           </p>
-           <br></br>
-           <li><p className="howText"> Using our web application, monitoring your machinery is simple and worry-free! Simply dispose of all food or animal wastes within the digester and let your power bill and the environment reap the benefits</p></li>
-            <br></br>
-            <br></br>
-            </li>
-            </ul>
-            <p className="howTextBtm">
-              <br></br>
-           To begin your journey into economical and environmentally-friendly waste management with us here at Compost Incorporated, please <a href="/Contact">contact us.</a></p>
+        <MDBContainer className="aboutDiv">
             
-            </MDBContainer>
-             
-              </MDBCol>
-              </MDBRow>
-              </MDBContainer>
+           <img className="aboutImgOne" src="https://i.imgur.com/EqKq6Ko.jpg"></img>
+           <h1 className="aboutText">Helping bring equity to all!</h1>
+              <p className="aboutText">Urban farming and local produce are high in demand for their environmental footprint, freshness of produce, and disseminating of food deserts. Because of this, several organizations around Central Florida approach urban farming as a means of sustainability. Some farms are even used as a platform for health education and healthy food production in the process. At Grow Orlando, we’ve decided to use urban farming to harvest healthy and wealthy people.</p>
+        
+            <img className="aboutImgs" src="https://i.imgur.com/r7Ggc5n.jpg"></img>
+            <p className="aboutText">  When we piloted our program at the Tangelo Park YMCA, we gave each student a healthy plant. Their job was to bring it back in two weeks alive and healthy for $2. If, however, they were to tell us about the health properties of the plants (and add some marketing value to them) we would negotiate the rate. Two weeks later, only one student brought their plant and we Shark Tank’ed the transaction in front of the others. When he received $3.25 that week, the next week everyone had a plant – some of which we’ve never seen before! But, believe it or not, every student knew about the healthy qualities of the plant – and some of them added some unique pots and added stories to increase the value. We thought, ‘Wow! All we needed to do was add incentive and the students were able to educate themselves about health. Imagine if we attached this learning process to a sustainable business model?’ </p>
+            <img src="https://i.imgur.com/bYcb2s3.jpg" className="aboutImgs"></img>
+            <p className="aboutText"> To unlock the potential of the future, you have to focus on the development of our youth. What better way to teach the students the skills life demands than through some good hard work? What better way to raise a community than to provide jobs? What better way to instill a healthy wealthy mindset than to hire youth to farm fresh produce, teaching life skills along the way? What’s more, the money raised can be used to educate and address issues revolving around food insecurity.</p>
+            <img src="https://i.imgur.com/S1mTQ8I.jpg" className="aboutImgOne"></img>
+            <p className="aboutText"> 
+            <br></br>
+            With Grow Orlando, access to nutritious food comes from our farm, affordability is increased with financial stimulus and literacy, and the community gets to enjoy the harvest of student growth and produce! 
+            <br></br>
+            <br></br>
+            As a teacher, I found that my students were missing some of the core courses that the FCAT does not require for testing. Subjects like soft skills for interviewing, financial literacy, work maturity, and mindfulness were slipping through the cracks, and I could only do so much within my parameters as a teacher. So, with my visionary skills intact, I decided to create an organization that covers these lessons in a unique way. I knew that the more tangible the platform, the deeper the retention and value for most students so I looked for platforms that would provide all of these needs. Enter farming.</p>
+            <img src="https://i.imgur.com/JTwLF01.jpg" className="aboutImgs"></img>
+            <img src="https://i.imgur.com/UrGKsQJ.jpg" className="aboutImgs"></img>
+            <p className="aboutText"> They saw these newcomers as people who want free farm labor for the extraction of created product for the benefit of a cause that’s not their own. We asked our students several times if they wanted to join the urban farming practices of Growing Orlando, and they refused because they did not want to farm for someone else’s benefit for free.</p>
+            <img src="https://i.imgur.com/DJAsoOl.png" className="aboutImgOne"></img>
+            <img src="https://i.imgur.com/27hGiZC.png" className="aboutImgOne"></img>
+            <p className="aboutText"> When I saw the demand for local produce and the environmental footprint urban farms develop, I found an arena that I could work with. I thought, ‘’ Besides, that’s what I did. As a matter of fact, almost every successful person I talked with started their careers mowing lawns, shoveling snow, picking crops, etc. The use of urban farming as an means for the development of a healthy and wealthy mindset in the community reaches far more solutions within that community than farming solely for the purpose of generating local produce and teaching health.</p>
+            <MDBRow>
+              <MDBCol size="5"></MDBCol>
+              <MDBCol size="2"><MDBBtn className="learnMore" color="white">learn more</MDBBtn></MDBCol>
+              <MDBCol size="5"></MDBCol>
+            </MDBRow>
+        
+        </MDBContainer>
                     
                   
     
