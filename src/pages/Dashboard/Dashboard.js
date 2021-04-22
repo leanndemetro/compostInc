4b5128574460bfe3abaa06bdafb825b0241e6259
Alyssa DeMetro <leanndemetro@gmail.com>
2021-04-21 21:45:12 -0400

    minor changes for mobile responsivness

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -10,8 +10,8 @@ function Dashboard() {
   return (
     <MDBContainer>
       <MDBRow>
-        <MDBCol size="3"></MDBCol>
-      <MDBCol size="9" className="dashCard">
+        <MDBCol size="4"></MDBCol>
+      <MDBCol size="8" className="dashCard">
         <div className="centered">
               <h1 className="dashText">Help us use plants to grow the lives of local teens!</h1>
               <p className="dashTextTwo">Here at Grow Orlando it is our mission to connect students with the world around them. Through access to our permaculture based local gardens, and training programs, we aim to educate students with lifelong gardening or even job skills.</p>
