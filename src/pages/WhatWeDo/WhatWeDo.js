a17fa0737349eee581a784ca1d1384250175845f
Alyssa DeMetro <leanndemetro@gmail.com>
2021-04-27 19:06:37 -0400

    about page updated, still need to add thanks to our partners area

--- src/pages/WhatWeDo/WhatWeDo.js
+++ src/pages/WhatWeDo/WhatWeDo.js
@@ -1,48 +1,77 @@
 import React from "react";
 import {
-  MDBContainer, MDBBtn, MDBRow, MDBCol, MDBIcon, MDBAnimation} from "mdbreact";
+  MDBContainer, MDBBtn, MDBRow, MDBCol, MDBIcon, MDBAnimation
+} from "mdbreact";
 import "./style.css"
- 
 
 
 
 
 
-function WhatWeDo ()  {
-    return (
-        <MDBContainer className="aboutDiv">
-            
-           <img className="aboutImgTop" src="https://i.imgur.com/EqKq6Ko.jpg"></img>
-           <h1 className="aboutText">Helping bring equity to all!</h1>
-              <p className="aboutText">Urban farming and local produce are high in demand for their environmental footprint, freshness of produce, and disseminating of food deserts. Because of this, several organizations around Central Florida approach urban farming as a means of sustainability. Some farms are even used as a platform for health education and healthy food production in the process. At Grow Orlando, we’ve decided to use urban farming to harvest healthy and wealthy people.</p>
-       <img className="aboutImgs" src="https://i.imgur.com/r7Ggc5n.jpg"></img>
-            <p className="aboutText">  When we piloted our program at the Tangelo Park YMCA, we gave each student a healthy plant. Their job was to bring it back in two weeks alive and healthy for $2. If, however, they were to tell us about the health properties of the plants (and add some marketing value to them) we would negotiate the rate. Two weeks later, only one student brought their plant and we Shark Tank’ed the transaction in front of the others. When he received $3.25 that week, the next week everyone had a plant – some of which we’ve never seen before! But, believe it or not, every student knew about the healthy qualities of the plant – and some of them added some unique pots and added stories to increase the value. We thought, ‘Wow! All we needed to do was add incentive and the students were able to educate themselves about health. Imagine if we attached this learning process to a sustainable business model?’ </p>
-            <img src="https://i.imgur.com/S1mTQ8I.jpg" className="aboutImgOne"></img>
-            <p className="aboutText"> To unlock the potential of the future, you have to focus on the development of our youth. What better way to teach the students the skills life demands than through some good hard work? What better way to raise a community than to provide jobs? What better way to instill a healthy wealthy mindset than to hire youth to farm fresh produce, teaching life skills along the way? What’s more, the money raised can be used to educate and address issues revolving around food insecurity.</p>
-            <img src="https://i.imgur.com/bYcb2s3.jpg" className="aboutImgs"></img>
-            <p className="aboutText"> 
-            With Grow Orlando, access to nutritious food comes from our farm, affordability is increased with financial stimulus and literacy, and the community gets to enjoy the harvest of student growth and produce! 
-            <br></br>
-            As a teacher, I found that my students were missing some of the core courses that the FCAT does not require for testing. Subjects like soft skills for interviewing, financial literacy, work maturity, and mindfulness were slipping through the cracks, and I could only do so much within my parameters as a teacher. So, with my visionary skills intact, I decided to create an organization that covers these lessons in a unique way. I knew that the more tangible the platform, the deeper the retention and value for most students so I looked for platforms that would provide all of these needs. Enter farming.</p>
-            <img src="https://i.imgur.com/27hGiZC.png" className="aboutImgOne"></img>
-            <img src="https://i.imgur.com/UrGKsQJ.jpg" className="aboutImgOne"></img>
-            <p className="aboutText"> They saw these newcomers as people who want free farm labor for the extraction of created product for the benefit of a cause that’s not their own. We asked our students several times if they wanted to join the urban farming practices of Growing Orlando, and they refused because they did not want to farm for someone else’s benefit for free.</p>
-            <p className="aboutText"> When I saw the demand for local produce and the environmental footprint urban farms develop, I found an arena that I could work with. I thought, ‘’ Besides, that’s what I did. As a matter of fact, almost every successful person I talked with started their careers mowing lawns, shoveling snow, picking crops, etc. The use of urban farming as an means for the development of a healthy and wealthy mindset in the community reaches far more solutions within that community than farming solely for the purpose of generating local produce and teaching health.</p>
-            <MDBRow>
-              <MDBCol size="5"></MDBCol>
-              <MDBCol size="7"> <MDBBtn color="none"><a href="/OurTeam">our team <MDBIcon icon="angle-double-right" /></a></MDBBtn></MDBCol>
-            </MDBRow>
-           
-              
-        
-        </MDBContainer>
-                    
-                  
-    
 
-      
+function WhatWeDo() {
+  return (
+    <MDBContainer className="aboutDiv">
+      <MDBRow>
+        <MDBCol className="aboutTitle">
+          <h1>Helping bring equity to all!</h1>
+          <br></br>
+          <h5>Food Apartheid: The systematic destruction of black self-determination to control one’s food, hyper-saturation of destructive foods and predatory marketing, and blatantly discriminatory corporate controlled food system that results in [communities of color] suffering from some of the highest rates of heart disease and diabetes of all time.</h5>
+        </MDBCol>
+      </MDBRow>
+      <MDBRow>
+        <MDBCol size="3"><img className="tiledImgs" src="https://i.imgur.com/wJdgRuU.jpg"></img></MDBCol>
+        <MDBCol size="3"><img className="tiledImgs" src="https://i.imgur.com/wfZrLJ4.jpg"></img></MDBCol>
+        <MDBCol size="3"><img className="tiledImgs" src="https://i.imgur.com/8F5fpN0.jpg"></img></MDBCol>
+        <MDBCol size="3"><img className="tiledImgs" src="https://i.imgur.com/JEZKUzz.jpg"></img></MDBCol>
+      </MDBRow>
+      <MDBRow>
+        <MDBCol size="10">
+          <br></br>
+          <br></br>
+          <img className="growItForward" src="https://i.imgur.com/EHwqkns.jpg"></img>
+        </MDBCol>
+      </MDBRow>
+      <MDBRow><MDBCol>
+        <div className="textWrap">
+        <img className="tiledImgs" src="https://i.imgur.com/EqKq6Ko.jpg"></img>
+          <img className="tiledImgs" src="https://i.imgur.com/27hGiZC.png"></img>
+          <img className="tiledImgs" src="https://i.imgur.com/EX3yYsy.jpg"></img>
+          </div>
+          <h1 className="aboutTextBottom">
+            <br></br>Grow it Forward</h1><p className="aboutText"> is a network of micro farms in Orange County where volunteers from congregations and communities work side-by-side with paid student farmers under the guidance of a Grow Orlando farm coordinator. Grow Orlando hires youth from underserved communities who gain work experience, financial literacy and life skills while cultivating their own seeds of potential.
+<br></br>
+<br></br>
+<h4 className="aboutTextBottom">Vision:</h4>
+To eradicate food disparity in low wealth and under resourced communities
+<br></br>
+<br></br>
+<h4 className="aboutTextBottom">Mission:</h4>
+To advance economic and food sovereignty through a self-sustaining micro farm network, utilizing available land of faith and community partners
+<br></br>
+<br></br>
+<h4 className="aboutTextBottom">Expansion:</h4>
+ St. Luke’s Farm broke ground September 2020 as a pilot and model for the Grow it Forward network. Our 2021 plans include four new farm sites as Grow it Forward moves into Food Priority Areas in the West Orange County area.
+Friends of the Farm: We are grateful to all for encouragement, marketing and funding the fruits of our labor:
+No Kid Hungry, Dr. Phillips Rotary, Pammies Sammies, Edible Orlando, West Orange Observer, Spectrum News 13, and the Eagle Scout program
+<br></br>
+<br></br>
+<h4 className="aboutTextBottom">Support our Growth:</h4> Shop our Saturday market or donate to our cause at st.lukes.org/growitforward. Click the GIVE button; choose Restoring Hope on the drop-down menu; and add “farm” in the notes section.
+<br></br>
+<br></br>
+<h4 className="aboutTextBottom">Contact:</h4>
+To be on our mailing list or for more information, contact Amy Winslow at awinslow@st.lukes.org 407.876.4991, ext. 297</p></MDBCol>
 
-    );
+      </MDBRow>
+
+    </MDBContainer>
+
+
+
+
+
+
+  );
 }
 
 export default WhatWeDo;
