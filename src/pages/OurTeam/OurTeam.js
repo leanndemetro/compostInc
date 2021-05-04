8b6e6c1a37d6f1c33e6cc7b737da626692f4581f
Alyssa DeMetro <leanndemetro@gmail.com>
2021-05-04 11:36:11 -0400

    arrow bullets added to out team page

--- src/pages/OurTeam/OurTeam.js
+++ src/pages/OurTeam/OurTeam.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { MDBRow, MDBCol, MDBCard, MDBCardBody, MDBIcon, MDBBtn, MDBContainer } from "mdbreact";
+import { MDBRow, MDBCol, MDBIcon, MDBContainer } from "mdbreact";
 import "./style.css";
 
 
@@ -13,7 +13,7 @@ function About() {
       <MDBRow className="text-md-left">
         <MDBCol lg="12" md="12" className="mb-5">
         <h2 className="h1-responsive font-weight-bold my-5">
-       The Team
+        <MDBIcon icon="caret-right" /> The Team 
         </h2>
           <MDBCol md="3" lg="3" className="float-left">
             <img
@@ -26,7 +26,7 @@ function About() {
           <MDBCol md="9" lg="9" className="float-right">
             <h4 className="font-weight-bold mb-3">Frank Bailey</h4>
             <h6 className="font-weight-bold mb-3">
-              Executive Director
+              Executive Director 
               </h6>
             <p>
               Frank M Bailey, Jr is the Executive Director and founder of Grow Orlando, Inc., an organization dedicated to teaching life, work, and financial literacy skills through plant cultivation. An Orlando-native, Frank grew up in Holden Heights and Metro West for the first half of his life, with the second half of his life residing in places like Gotha, Winter Park, and Winter Garden. Living and understanding the lives that live on ‘both sides of the tracks’, Frank has dedicated his life to using his ideas as a visionary to develop education and empowerment, creating activities and programs that fill the gap of opportunity and experience in places that need it.  He is also currently sitting as the Chairman of the Advisory Board for the Winter Garden Community Garden, is currently a CRAVE leader, and speaks at seminars, churches, conferences, support groups, clubs, and one on one with You!
@@ -224,7 +224,7 @@ function About() {
 
         <MDBCol lg="12" md="12" className="mb-5">
         <h2 className="h1-responsive font-weight-bold my-5">
-       The Board
+        <MDBIcon icon="caret-right" /> The Board
         </h2>
           <MDBCol md="3" lg="3" className="float-right">
             <img
