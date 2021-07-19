f455d715350cea626c72680bb35635ecabe615ea
Alyssa DeMetro <leanndemetro@gmail.com>
2021-07-19 17:37:38 -0400

    whatwedo page updated, grow it forward info still needed but fully mobile responsive

--- src/pages/WhatWeDo/WhatWeDo.js
+++ src/pages/WhatWeDo/WhatWeDo.js
@@ -13,21 +13,17 @@ import GrowItForward from "../../components/GrowItForward/index";
 function WhatWeDo() {
   return (
     <div>
-    <MDBContainer>
-     <MDBRow>
-     <MDBCol><img src="https://lh3.googleusercontent.com/_rTKnvj-ptsoCVHWizuFGyEvIQkZq0cz_vJiKsdtbud86ycEwn6a33CQvHYnNc_ruFxJURTZ3swCwPH3eSft_6q1CasSK01GSjCyX8VhLMgJrcM8n5LIPyO15rNafsE-s6mDTwC0M7aCvhT0Q1lsOujKb8fdYbloyDFz_7mmRUD8R0pJgS1GtadSD0-TXsdeucPTy9DqCuZTFRnuKFLEcQz5jwUkhwkQi3ssOu_12rSOZ-qOave_BkeGvOnIZULLmiAP6EFZ_7at3OIV2MTtK1N7D4r2eagj3Zk3rVHJrI_b9viLZo3-zWPKMhilIzRlSXF-H6BgJtAfJ3Vz05Me6y7SfNTK1WElR5vLs3VAxv1u_E1cSQFyQwDgSgBZsvygGpEpar0ob3dVi3tNLNK6q2f1NgaaZ-gkIKcxuQvrzQsk70lAed73fzUJK2iOTTjQTS8x9CmBv-NMgQ27Wl9ZpgaXcJV7Exjjj4qHWyDLYmPLMu-M5bpAT2AfWkEOdoMffel6MDKkXnt86f70bMPR8pavR5SBQvbYYSOOoTaG1NYpxeNHgx4iAWEaegSZLG0i7T83XEHPc97lvT5J5VsMAc4yEuLFSIqQfj6iXOo9CE66cIhcwhwUSuYpUPSj-NcRtqKfSUMiHrBz59c1R46rAsDwr2tiEMpER6IHuGFqWtMHllVpNDB_o6gSxw5FeDewESuEJL1bhn_e1cW9u2CrxFf0=w1658-h1184-no?authuser=0" className="aboutImage"></img></MDBCol>
-     <MDBCol>
-       <p>WHAT WE DO
+    <img className="textWrap aboutImage" src="https://lh3.googleusercontent.com/_rTKnvj-ptsoCVHWizuFGyEvIQkZq0cz_vJiKsdtbud86ycEwn6a33CQvHYnNc_ruFxJURTZ3swCwPH3eSft_6q1CasSK01GSjCyX8VhLMgJrcM8n5LIPyO15rNafsE-s6mDTwC0M7aCvhT0Q1lsOujKb8fdYbloyDFz_7mmRUD8R0pJgS1GtadSD0-TXsdeucPTy9DqCuZTFRnuKFLEcQz5jwUkhwkQi3ssOu_12rSOZ-qOave_BkeGvOnIZULLmiAP6EFZ_7at3OIV2MTtK1N7D4r2eagj3Zk3rVHJrI_b9viLZo3-zWPKMhilIzRlSXF-H6BgJtAfJ3Vz05Me6y7SfNTK1WElR5vLs3VAxv1u_E1cSQFyQwDgSgBZsvygGpEpar0ob3dVi3tNLNK6q2f1NgaaZ-gkIKcxuQvrzQsk70lAed73fzUJK2iOTTjQTS8x9CmBv-NMgQ27Wl9ZpgaXcJV7Exjjj4qHWyDLYmPLMu-M5bpAT2AfWkEOdoMffel6MDKkXnt86f70bMPR8pavR5SBQvbYYSOOoTaG1NYpxeNHgx4iAWEaegSZLG0i7T83XEHPc97lvT5J5VsMAc4yEuLFSIqQfj6iXOo9CE66cIhcwhwUSuYpUPSj-NcRtqKfSUMiHrBz59c1R46rAsDwr2tiEMpER6IHuGFqWtMHllVpNDB_o6gSxw5FeDewESuEJL1bhn_e1cW9u2CrxFf0=w1658-h1184-no?authuser=0"></img>
+        <p className="aboutText">WHAT WE DO
             <hr></hr>
             When we piloted our program at the Tangelo Park YMCA, we gave each student a healthy plant. Their job was to bring it back in two weeks alive and healthy for $2. If, however, they were to tell us about the health properties of the plants (and add some marketing value to them) we would negotiate the rate.
               <br></br>
               <br></br>
               Two weeks later, only one student brought their plant and we Shark Tank'd the transaction in front of the others. When he received $3.25 that week, the next week everyone had a plant - some of which we’ve never seen before! But, believe it or not, every student knew about the healthy qualities of the plant - and some of them added some unique pots and added stories to increase the value. We thought, ‘Wow! All we needed to do was add incentive and the students were able to educate themselves about health.’</p>
-     </MDBCol>
-     </MDBRow>
-     <MDBRow>
-     <MDBCol>
-       <p>BUT HOW DO WE MAKE IT EQUITABLE?
+   
+    <img className="textWrapLeft aboutImage" src="https://lh3.googleusercontent.com/dsDFzdPI7jkhAEOM3tKAPu0IGOQuXLvVtxAEMHns8FjC-omVa5XcJIuoN1suYVFScOXQctaWnGOgrRNfHF-q-kG6srcIuXM1k69M2cBz3XwqN1xJXrGiwLEm57U6r_LP_t2s9qGLtm4l1uHr_ok6kXRdp6xL3BBakFkJRZZCB4NUgDyhiqrMt9C7fCn0JM3TCz-X_bnRFglu8gpzz3yBdZNaI_Fr8gWITV8u-P6afn26IiP7-4KTBrL4kYl5nln7rxOnyompIOzObqfau2-liDhiJ6G_F0NWpvycPkQZPcEsp9-TGmFyMeTp7YnMoFdFWeoHW_aX-ibitqf0Rs2rUJ2WoOf0q1Q-f3G4smvfl8ScRj0UZ4v2vZcesjb-JX07d4nNlTVPeLWPZ6tuqETv_A8AORxMPXlCZtvnkx8A0heKf9jDnFlCrJPVCrlmceAIKUNtCNiVrbSUHo8Ar3xDnS74jniJ8uX2n2odKkQzjlAzlWUDA5FRRtTuKdM4719i97ba3aRQnJIQSrvlkY4GBuGVSqa136cXMSmAMK1mIaIgGyzHaWlpt8Z4HaFZqtvHKbVpTogOBg_CBqpcmZMm880hP_Nb7RL_k-PxV3O8biZ2DVkc0AjOUBVL4xbhZgzwlgTTC-uqNN6acabhrdeZG6comoxcm-ipRmH_l8u3IVX1IuYtYAO_LnQyBHnVmUGQj6-NDv7q2n87kM9FjNah-_-x=w1658-h1184-no?authuser=0"></img>
+        <p className="aboutTextTwo">
+        BUT HOW DO WE MAKE IT EQUITABLE?
             <hr></hr>
             With the demand for local produce and the environmental footprint urban farms develop, there is an arena that we work with that would make it equitable. Besides, that’s what I did. As a matter of fact, almost every successful person I talked with started their careers mowing lawns, shoveling snow, picking crops for grandma, etc.
             </p>
@@ -43,25 +39,12 @@ function WhatWeDo() {
               <br></br>
               What better way to instill a healthy wealthy mindset than to hire youth to farm fresh produce, teaching life skills along the way?
             </p>
-     </MDBCol>
-     <MDBCol><img src="https://lh3.googleusercontent.com/dsDFzdPI7jkhAEOM3tKAPu0IGOQuXLvVtxAEMHns8FjC-omVa5XcJIuoN1suYVFScOXQctaWnGOgrRNfHF-q-kG6srcIuXM1k69M2cBz3XwqN1xJXrGiwLEm57U6r_LP_t2s9qGLtm4l1uHr_ok6kXRdp6xL3BBakFkJRZZCB4NUgDyhiqrMt9C7fCn0JM3TCz-X_bnRFglu8gpzz3yBdZNaI_Fr8gWITV8u-P6afn26IiP7-4KTBrL4kYl5nln7rxOnyompIOzObqfau2-liDhiJ6G_F0NWpvycPkQZPcEsp9-TGmFyMeTp7YnMoFdFWeoHW_aX-ibitqf0Rs2rUJ2WoOf0q1Q-f3G4smvfl8ScRj0UZ4v2vZcesjb-JX07d4nNlTVPeLWPZ6tuqETv_A8AORxMPXlCZtvnkx8A0heKf9jDnFlCrJPVCrlmceAIKUNtCNiVrbSUHo8Ar3xDnS74jniJ8uX2n2odKkQzjlAzlWUDA5FRRtTuKdM4719i97ba3aRQnJIQSrvlkY4GBuGVSqa136cXMSmAMK1mIaIgGyzHaWlpt8Z4HaFZqtvHKbVpTogOBg_CBqpcmZMm880hP_Nb7RL_k-PxV3O8biZ2DVkc0AjOUBVL4xbhZgzwlgTTC-uqNN6acabhrdeZG6comoxcm-ipRmH_l8u3IVX1IuYtYAO_LnQyBHnVmUGQj6-NDv7q2n87kM9FjNah-_-x=w1658-h1184-no?authuser=0" className="aboutImage"></img>
-      </MDBCol>
-     </MDBRow>
-     <MDBRow>
-     <MDBCol><img src="https://lh3.googleusercontent.com/DfbAsT8-JS3PQK755XCpX3bA2k18FE0d3lgzSz-v2PGr9Jzk2Se2xce-nBhfKDZke1jSZaoVUxyV9ZMb9UohWFJhrLP5L0K16fthNurkMGsXQB3XyehwECqtPPGjTUTzFmVcGOrzebAJce--RMwDeeJP0H9_fbQIh6RSgMCKeD3OLN9z-dJ6-K95b9wCz4nwvzkjgxKFluCW_TaGY4AwOflUnEt7sCJtlnoiomfbXnLQFuGYxgvavxK55Oah9HRa8d9qAwWgE2d7OIB7axVfsYcstlPvocfYPYGeVwQhVMKxA9N3T4YjhRAelxx-vedLmbkwuE_yYu7SrPWJRkw8lg996I_wCR5FyTvdPnm0VotJg2CrKba-ujGMN2ZBsOywH8dquAMvo6-GttINmnKtWWLXYQkhZLsPmZINLbxAqZdPyUM--PljrrgRG7113Or8EVgkdVgL_GYKLqwBnZdtiJsXVuK-hQOeb-BHljYqRTXhtKld0jQTqjb0pGJMs4MwEz-YTLeux80hU_CCa15B5f7s7N3-RkWclfdPlF1r33n8Ypffy_AYy1NogC9WsfItJinkz5XNMbydxfYhT3LPLxUJGKdcBRHwzA4Ec869v4Mi-5JHeL1nlA3R3veOECkt5zK8Vsfi4RB-86UbQMfJnnMCN7Zfm0t1RfX9BMjv9oHdjYZEOsHQWIFaa3DFp6VZl345e21C3zgqiKLfc1AzBKtF=w1658-h1184-no?authuser=0" className="aboutImage"></img></MDBCol>
-     <MDBCol>
-       <p><img className="growLogo" src="https://i.imgur.com/gCIu3Wl.png"></img>
+            <br></br>
+        <p className="aboutTextThree"><img className="growLogo" src="https://i.imgur.com/gCIu3Wl.png"></img>
          <br></br>
-            With Grow Orlando, access to nutritious food comes from our farm, affordability is increased with financial stimulus and literacy, and the community gets to enjoy the harvest of healhy produce sourced locally from their own youth and neighborhood farmers.</p>
-     </MDBCol>
-     </MDBRow>
-     <MDBRow>
-     <MDBCol>
-       <p>text</p>
-     </MDBCol>
-     <MDBCol><img src="https://lh3.googleusercontent.com/uZiose-eX5CbEXBaTq6vSlRyFQ0fdGQJCfOUCtOuyKDZLN0s6Xc4KoystKcxYXXxuyCGhOcq9XVavuCUDkln-93hrGd9VAOoJMLh6ZouA-k--l_k3uSl-RwgXxCoeFto6rNvhrkyPizRUzjQB2gAZr6sqmZd5DWbEHTcznCZcjiC3AjiOQxsXEdMdnXCrZ-cjXhFa04xdEFDPO4_Vwm248elkjE-yY5p0MgTvxT3pwhpmY8sO2N0FOmbVRzzD0ixhfz2w7oVlUXh27-WH7pwJGo_4c3QTcI8tiem6-T1WZewNw2TPn_XlYHhBFWSK-th0tBCFzCwEH2BCG1bCGIVUxwUdoCC-niKocPjN9piiSCXx8HLlh3wMOXDFNXHNcXyWN2YoJmYXNLZG62lHsamvSP3gcmVp99zhbeCZlQZCPoi-vt78r-jTmzJ1_crfLAYuM7gMk7RzLg8AyR0MfA8bmBTI_wo0rdhFUK5pXGQkpOxIX1uv8NAYHZJkpsGCgJxv4nkGGwPJonrkPF2bTFXXBuBpmOA5an7i6Q3fuXc-xkR8HYeUlm4vX1xBWcoQOwxu1J7-GiNsMW89sAP7lJ6qvxWpy-sPjv5iuuMpc-KpdUtcyxC9yOhtrVBFHnNuPGR4mMOdDOu5wOVBZ0owf2FPYg6wetYiU-qps0Fe3FZ0UXDBfCEL1MiduaOerRuAsTuYS2dTlmWeK4iCo4LJLWZUsjI=w1658-h1184-no?authuser=0" className="aboutImage"></img></MDBCol>
-     </MDBRow>
-    </MDBContainer>
+            With Grow Orlando, access to nutritious food comes from our farm, affordability is increased with financial stimulus and literacy, and the community gets to enjoy the harvest of healhy produce sourced locally from their own youth and neighborhood farmers.
+            </p>
+
     </div>
 
 
