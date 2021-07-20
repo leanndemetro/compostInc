ad0082255846bd2e7da6d2044feecde867179da7
Alyssa DeMetro <leanndemetro@gmail.com>
2021-07-20 08:34:07 -0400

    img links updated to imgur

--- src/pages/WhatWeDo/WhatWeDo.js
+++ src/pages/WhatWeDo/WhatWeDo.js
@@ -13,7 +13,7 @@ import GrowItForward from "../../components/GrowItForward/index";
 function WhatWeDo() {
   return (
     <div>
-    <img className="textWrap aboutImage" src="https://lh3.googleusercontent.com/_rTKnvj-ptsoCVHWizuFGyEvIQkZq0cz_vJiKsdtbud86ycEwn6a33CQvHYnNc_ruFxJURTZ3swCwPH3eSft_6q1CasSK01GSjCyX8VhLMgJrcM8n5LIPyO15rNafsE-s6mDTwC0M7aCvhT0Q1lsOujKb8fdYbloyDFz_7mmRUD8R0pJgS1GtadSD0-TXsdeucPTy9DqCuZTFRnuKFLEcQz5jwUkhwkQi3ssOu_12rSOZ-qOave_BkeGvOnIZULLmiAP6EFZ_7at3OIV2MTtK1N7D4r2eagj3Zk3rVHJrI_b9viLZo3-zWPKMhilIzRlSXF-H6BgJtAfJ3Vz05Me6y7SfNTK1WElR5vLs3VAxv1u_E1cSQFyQwDgSgBZsvygGpEpar0ob3dVi3tNLNK6q2f1NgaaZ-gkIKcxuQvrzQsk70lAed73fzUJK2iOTTjQTS8x9CmBv-NMgQ27Wl9ZpgaXcJV7Exjjj4qHWyDLYmPLMu-M5bpAT2AfWkEOdoMffel6MDKkXnt86f70bMPR8pavR5SBQvbYYSOOoTaG1NYpxeNHgx4iAWEaegSZLG0i7T83XEHPc97lvT5J5VsMAc4yEuLFSIqQfj6iXOo9CE66cIhcwhwUSuYpUPSj-NcRtqKfSUMiHrBz59c1R46rAsDwr2tiEMpER6IHuGFqWtMHllVpNDB_o6gSxw5FeDewESuEJL1bhn_e1cW9u2CrxFf0=w1658-h1184-no?authuser=0"></img>
+    <img className="textWrap aboutImage" src="https://i.imgur.com/dGVH2G9.jpg"></img>
         <p className="aboutText">WHAT WE DO
             <hr></hr>
             When we piloted our program at the Tangelo Park YMCA, we gave each student a healthy plant. Their job was to bring it back in two weeks alive and healthy for $2. If, however, they were to tell us about the health properties of the plants (and add some marketing value to them) we would negotiate the rate.
@@ -21,7 +21,7 @@ function WhatWeDo() {
               <br></br>
               Two weeks later, only one student brought their plant and we Shark Tank'd the transaction in front of the others. When he received $3.25 that week, the next week everyone had a plant - some of which we’ve never seen before! But, believe it or not, every student knew about the healthy qualities of the plant - and some of them added some unique pots and added stories to increase the value. We thought, ‘Wow! All we needed to do was add incentive and the students were able to educate themselves about health.’</p>
    
-    <img className="textWrapLeft aboutImage" src="https://lh3.googleusercontent.com/dsDFzdPI7jkhAEOM3tKAPu0IGOQuXLvVtxAEMHns8FjC-omVa5XcJIuoN1suYVFScOXQctaWnGOgrRNfHF-q-kG6srcIuXM1k69M2cBz3XwqN1xJXrGiwLEm57U6r_LP_t2s9qGLtm4l1uHr_ok6kXRdp6xL3BBakFkJRZZCB4NUgDyhiqrMt9C7fCn0JM3TCz-X_bnRFglu8gpzz3yBdZNaI_Fr8gWITV8u-P6afn26IiP7-4KTBrL4kYl5nln7rxOnyompIOzObqfau2-liDhiJ6G_F0NWpvycPkQZPcEsp9-TGmFyMeTp7YnMoFdFWeoHW_aX-ibitqf0Rs2rUJ2WoOf0q1Q-f3G4smvfl8ScRj0UZ4v2vZcesjb-JX07d4nNlTVPeLWPZ6tuqETv_A8AORxMPXlCZtvnkx8A0heKf9jDnFlCrJPVCrlmceAIKUNtCNiVrbSUHo8Ar3xDnS74jniJ8uX2n2odKkQzjlAzlWUDA5FRRtTuKdM4719i97ba3aRQnJIQSrvlkY4GBuGVSqa136cXMSmAMK1mIaIgGyzHaWlpt8Z4HaFZqtvHKbVpTogOBg_CBqpcmZMm880hP_Nb7RL_k-PxV3O8biZ2DVkc0AjOUBVL4xbhZgzwlgTTC-uqNN6acabhrdeZG6comoxcm-ipRmH_l8u3IVX1IuYtYAO_LnQyBHnVmUGQj6-NDv7q2n87kM9FjNah-_-x=w1658-h1184-no?authuser=0"></img>
+    <img className="textWrapLeft aboutImage" src="https://i.imgur.com/8rIMPNO.jpg"></img>
         <p className="aboutTextTwo">
         BUT HOW DO WE MAKE IT EQUITABLE?
             <hr></hr>
