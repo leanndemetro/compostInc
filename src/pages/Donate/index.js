a1b562a5e6b3141374791abf8b5877431000ab0e
Alyssa DeMetro <leanndemetro@gmail.com>
2021-07-20 14:08:10 -0400

    donate page rough draft complete without stripe form

--- src/pages/Donate/index.js
+++ src/pages/Donate/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./style.css";
-import  { MDBIcon } from "mdbreact";
+import  { MDBContainer, MDBRow, MDBCol, MDBIcon, MDBBtn, MDBInput} from "mdbreact";
 
 
 
@@ -15,28 +15,24 @@ function Donate() {
   };
 
   return (
-    
-   
-
-
-   <div className="donate__card">
-   <div className="donate__widget">
-       <img src="https://i.imgur.com/gCIu3Wl.png" className="donate__widget__image"></img>
-       <div className="widget__body">
-        <p className="widget__header">
-            When you donate, you help sow the seeds of potential in a child's life
-        </p>
-        <label className="widget__input__label" for="widget_input">$</label>
-        <input labelfor="$" className="widget__input" type="number"></input>
-        <input placeholder="Enter Amount" value="Give Now!" type="submit" class="widget__submit"></input>
-            <br></br>
-            <a target="_blank" className="icon__link" href="https://stripe.com/"><MDBIcon className="stripe__icon" fab icon="stripe" /></a>
-
-       </div>
-       </div>
-       
-   </div>
-        
+    <div>
+   <MDBContainer>
+     <img className="donateImg" src="https://i.imgur.com/28uIy3J.jpg"></img>
+     <p>
+       <hr className="donateNow"></hr>
+       DONATE NOW<MDBIcon icon="chevron-circle-right arrow" />
+       <hr></hr></p>
+     <MDBRow>
+       <MDBCol><img className="donateImg" src="https://i.imgur.com/DFxtlOu.jpg"></img></MDBCol>
+       <MDBCol><img className="donateImg" src="https://i.imgur.com/sSQe6JO.jpg"></img></MDBCol>
+     </MDBRow>
+     <br></br>
+     <MDBRow>
+       <MDBCol><img className="donateImg" src="https://i.imgur.com/jEaw389.jpg"></img></MDBCol>
+       <MDBCol><img className="donateImg" src="https://i.imgur.com/D4qNOZc.jpg"></img></MDBCol>
+     </MDBRow>
+     </MDBContainer>
+      </div>  
 
        
     
